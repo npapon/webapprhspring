75b81debb76d55bc453c829d8f5f7a3984fdace1
Papoune <npapon@live.fr>
2021-02-26 13:21:59 +0100

    First commit

---
+++ src/main/java/fr/papounworld/webapprh/WebapprhApplication.java
@@ -0,0 +1,13 @@
+package fr.papounworld.webapprh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebapprhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebapprhApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/papounworld/webapprh/WebapprhApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.papounworld.webapprh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebapprhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
