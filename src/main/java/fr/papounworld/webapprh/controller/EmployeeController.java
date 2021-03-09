80ff7e2c5f3978bf46bbfe45c6e73e4a4b76b8ae
Papoune <npapon@live.fr>
2021-03-09 13:59:26 +0100

    Création et mise à jour employee

--- src/main/java/fr/papounworld/webapprh/controller/EmployeeController.java
+++ src/main/java/fr/papounworld/webapprh/controller/EmployeeController.java
@@ -4,7 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import fr.papounworld.webapprh.model.Employee;
@@ -18,7 +20,7 @@ public class EmployeeController {
 	
 	 //L’objet Model (org.springframework.ui.Model) a été ajouté en paramètre de la méthode home(). 
 	 //Grâce à cela, Spring se charge de nous fournir une instance de cet objet.
-	@GetMapping("/home")
+	@GetMapping("/")
 	public String home(Model model) {
 		
 		//On récupére tous les employées en base
@@ -34,10 +36,31 @@ public class EmployeeController {
 	@GetMapping("/deleteEmployee/{id}")
 	public ModelAndView deleteEmployee(@PathVariable("id") final int id) {
 	    employeeService.deleteEmployee(id);
-	    return new ModelAndView("redirect:/home");
+	    return new ModelAndView("redirect:/");
 	}
 
 	
 	@GetMapping("/supervision")
 	public void supervision() {}
+	
+	@PostMapping("/saveEmployee")
+	public ModelAndView saveEmployee(@ModelAttribute Employee employee) {
+		employeeService.saveEmployee(employee);
+	    return new ModelAndView("redirect:/");
+	}
+	
+	@GetMapping("/createEmployee")
+	public String createEmployee(Model model) {
+		Employee employee = new Employee();
+		model.addAttribute("employee", employee);
+		return "creationEmploye";
+	}
+	
+	//on récupère l'employée dans un attribut et on arrive avec sur la page form
+	@GetMapping("/updateEmployee/{id}")
+	public String updateEmployee(@PathVariable("id") final int id, Model model) {
+		Employee employee = employeeService.getEmployee(id);		
+		model.addAttribute("employee", employee);	
+		return "miseAJourEmployee.html";		
+	}
 }
